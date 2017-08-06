c87087a25a2b3d109341d7f9535a1480adead1b3
Jasper Gerth <j.gerth@student.utwente.nl>
2017-08-06 14:59:43 +0200

    input tester added and changed volume control from buttons to rotary encoder

---
+++ gpiotest.py
@@ -0,0 +1,56 @@
+try:
+ import RPi.GPIO as GPIO
+except ImportError:
+    print("Cannot import RPi.GPIO")
+from time import sleep
+class gpiotest():
+
+    def __init__(self):
+        #Set bouncetime
+        deb_time = 50 #for normal pushbuttons
+        vol_deb_time = 10 #for the voluem rotary encoder
+
+        self.pins={}
+        self.pins['play_pin'] = 4
+        self.pins['vol_A_pin'] = 17
+        self.pins['vol_B_pin'] = 18
+
+        #Set pin mode.
+        GPIO.setmode(GPIO.BCM)
+
+        GPIO.setup(self.pins['play_pin'], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(self.pins['play_pin'], GPIO.FALLING, callback=self.play, bouncetime=deb_time)
+
+        GPIO.setup(self.pins['vol_A_pin'], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.pins['vol_B_pin'], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(self.pins['vol_A_pin'], GPIO.BOTH, callback=self.volume, bouncetime=vol_deb_time)
+
+        print ("Init done.")
+        while True:
+            sleep(1)
+
+    def play(self,channel):
+        print("Play button pressed")
+
+    def volume(self,channel):
+        print("Volume event.")
+        if GPIO.input(channel)==1: #it was a rising edge
+            if GPIO.input(self.pins['vol_B_pin']) == 0: #clockwise movement
+                print ("Volume up!")
+            else: #counterclockwise movemnt
+                print ("Volume down")
+
+        else: # it was a falling edge.
+            if GPIO.input(self.pins['vol_B_pin']) == 1: #clockwise movement
+                print ("volume up")
+            else: #counterclockwise movement
+                print ("Volume down")
+
+print("calling that shizzle")
+gpiotest()
+
+
+
+
+
+
--- mopidy_gpiocont/input_gpio.py
+++ mopidy_gpiocont/input_gpio.py
@@ -4,12 +4,13 @@ import logging
 logger = logging.getLogger(__name__)
 logger.debug("GPIOcont: input_gpio.py called.")
 
-deb_time = 50 #The debounce time for the gpio input
+deb_time = 50 #The debounce time for the buttons
+vol_deb_time = 50 # The debounce time for the rotary encoder
 
 class input_GPIO():
     def __init__(self, frontend, pins):
         self.frontend = frontend
-
+        self.pins = pins
         try:
             # set pin mode to BCM
             GPIO.setmode(GPIO.BCM)
@@ -18,13 +19,10 @@ class input_GPIO():
             GPIO.setup(pins['play_pin'], GPIO.IN, pull_up_down=GPIO.PUD_UP)
             GPIO.add_event_detect(pins['play_pin'], GPIO.FALLING, callback=self.play, bouncetime=deb_time)
 
-            # Volume up button
-            GPIO.setup(pins['vol_up_pin'], GPIO.IN, pull_up_down=GPIO.PUD_UP)
-            GPIO.add_event_detect(pins['vol_up_pin'], GPIO.FALLING, callback=self.vol_up, bouncetime=deb_time)
-
-            # Volume down button
-            GPIO.setup(pins['vol_down_pin'], GPIO.IN, pull_up_down=GPIO.PUD_UP)
-            GPIO.add_event_detect(pins['vol_down_pin'], GPIO.FALLING, callback=self.vol_down, bouncetime=deb_time)
+            #Volume rotary encoder
+            GPIO.setup(pins['vol_A_pin'], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+            GPIO.setup(pins['vol_B_pin'], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+            GPIO.add_event_detect(pins['vol_A_pin'], GPIO.BOTH, callback=self.volume, bouncetime=vol_deb_time)
 
             logger.debug("GPIOcont: pin events added.")
         except RuntimeError:
@@ -33,8 +31,16 @@ class input_GPIO():
     def play(self, channel):
         self.frontend.input_event('play')
 
-    def vol_up(self,channel):
-        self.frontend.input_event('vol_up')
+    def volume(self,channel):
+        logger.debug("Volume event.")
+        if GPIO.input(channel)==1: #it was a rising edge
+            if GPIO.input(self.pins['vol_B_pin']) == 0: #clockwise movement
+                logger.debug ("Volume up!")
+            else: #counterclockwise movemnt
+                logger.debug("Volume down")
 
-    def vol_down(self,channel):
-        self.frontend.input_event('vol_down')
+        else: # it was a falling edge.
+            if GPIO.input(self.pins['vol_B_pin']) == 1: #clockwise movement
+                logger.debug("volume up")
+            else: #counterclockwise movement
+                logger.debug("Volume down")
